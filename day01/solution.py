624e3409faccf68bc257b53552a6f6ff50094877
wijnand-gritter <152634339+wijnand-gritter@users.noreply.github.com>
2024-12-01 20:56:41 +0100

    Create solution.py

---
+++ day01/solution.py
@@ -0,0 +1,15 @@
+with open("input.txt") as file:
+    data = file.read().strip().split("\n")
+
+matrix = [list(map(int, line.split("   "))) for line in data]
+
+rotated = [list(row) for row in zip(*matrix[::-1])]
+
+sorted_matrix = [sorted(row) for row in rotated]
+
+first_row, second_row = sorted_matrix[0], sorted_matrix[1]
+difference_sum = sum(abs(first_row[i] - second_row[i]) for i in range(len(first_row)))
+print(difference_sum) # Part 1
+
+similarity = sum(second_row.count(value) * value for value in first_row)
+print(similarity) # Part 2
